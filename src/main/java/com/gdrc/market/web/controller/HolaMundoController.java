30402d7a1da78f32d52120c93425b2478ec86571
Gabrieldrc <gabriel.drc25.2@gmail.com>
2021-06-15 13:52:14 -0300

    Se realizo la estructura donde esta distribuida por capa. La primera es dominio, donde estara DTO y objetos de dominio (son objetosde contexto que hacen parte de nuestra aplicacion), los servicios (son los puentes entre los controladores de la api y la capa de persistencia), especificaion de los repositorios (interfaces que difnen las reglas de juego o los contratos que debe cumplir la persistencia para intervenir entre los objetos de dominio y la base de datos); La segunda capa es web, donde estaran los controladores de nuestra api; Y la ultima capa es la persistencia, donde tenemos los repositorios (que son los que van a implementar las especificaciones que tenemos en el dominio) y los entities (son las clases que mapean y hacen las veces de las tablas de nuestra base de datos).

--- src/main/java/com/gdrc/market/web/controller/HolaMundoController.java
+++ src/main/java/com/gdrc/market/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.gdrc.market;
+package com.gdrc.market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;

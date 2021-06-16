a800153eed81577facbd4350c15d5ad1a972b2da
Gabrieldrc <gabriel.drc25.2@gmail.com>
2021-06-16 17:36:50 -0300

    Se añadio dos query methods al ProductCrudRepository y se llamaron en el ProductRepository.

--- src/main/java/com/gdrc/market/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/gdrc/market/persistence/crud/ProductCrudRepository.java
@@ -3,6 +3,9 @@ package com.gdrc.market.persistence.crud;
 import com.gdrc.market.persistence.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 /*
 Spring Data Repositories:
     Te ayuda a ahorrarte codigo y puedes hacer operaciones
@@ -21,5 +24,29 @@ Spring Data Repositories:
     Este CrudRepository debe extender de CrudRepository,
     indicandole la tabla y el tipo de la pk.
  */
+/*
+Uso de Query Methods
+    Cuando necesitamos hacer consultas que Spring Data no puede ofrecer.
+    Proveen la posibilidad de generar consultas mediante el nombre
+    de los metodos.
+    Tambien tiene la posibilidad de retornar Optional<T>
+    mas info:
+    https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+
+    Como ejemplo en la interfaz creamos un metodo donde colocamos:
+    findBy: representa el "WHERE"
+    CategoryId: la propiedad que representa la columna de la tabla
+    (hay que escribirlo tal cual, solo cambiando las mayusculas para
+    cumplir con el cammel case)
+    Y por parametro tiene que llamarse exactamente como la propiedad.
+
+    NOTA: Tambien podemos llamar como queramos al metodo y usar la
+    anotacion @Query con el valor que representa el query.
+
+ */
 public interface ProductCrudRepository extends CrudRepository<Product, Integer> {
+    //    @Query(value = "SELECT * FROM products WHERE category_id = ? ORDER BY ASC", nativeQuery = true)
+    List<Product> findByCategoryIdOrderByAsc(int categoryId);
+
+    Optional<List<Product>> findByStockQuantityLessThanAndState(int stockQuantity, boolean state);
 }

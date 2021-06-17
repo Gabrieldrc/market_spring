4adef21bee174470cf0e251e9c5ce6f9b9dd7aca
Gabrieldrc <gabriel.drc25.2@gmail.com>
2021-06-17 10:33:44 -0300

    Se añadio la anotacion Repository a la clase ProductRepository

--- src/main/java/com/gdrc/market/persistence/ProductRepository.java
+++ src/main/java/com/gdrc/market/persistence/ProductRepository.java
@@ -2,10 +2,17 @@ package com.gdrc.market.persistence;
 
 import com.gdrc.market.persistence.crud.ProductCrudRepository;
 import com.gdrc.market.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+/*
+@Repository:
+    Con esta anotacion le indicamos a Spring que esta clase se
+    encargara de interactuar directamente con la db.
+ */
+@Repository
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
@@ -19,5 +26,16 @@ public class ProductRepository {
 
     public Optional<List<Product>> getByQuantityLessThan(int stockQuantity) {
         return productCrudRepository.findByStockQuantityLessThanAndState(stockQuantity, true);
+    }
+
+    public Optional<Product> getProduct(int productId) {
+        return productCrudRepository.findById(productId);
+    }
+
+    public Product save(Product product) {
+        return productCrudRepository.save(product);
+    }
+    public void delete(int productId) {
+        productCrudRepository.deleteById(productId);
     }
 }

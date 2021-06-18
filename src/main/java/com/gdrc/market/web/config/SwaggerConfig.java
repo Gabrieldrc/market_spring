aeb37abb0f47949c6b7e397c32ea496b06d36660
Gabrieldrc <gabriel.drc25.2@gmail.com>
2021-06-18 19:43:59 -0300

    Se instalo las dependencias de swagger y se creo la clase de configuracion donde le indicamos donde se encontraban nuestros controladores. El enlace abre en /market/api/swagger-ui/index.html

---
+++ src/main/java/com/gdrc/market/web/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.gdrc.market.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/*
+@Configuration:
+    Es una anotacion de spring que indica que es
+    una clase de configuracion
+@EnableSwagger2:
+    Es una anotacion para habilitar Swagger2
+@Bean:
+    Indicates that a method produces a bean
+    to be managed by the Spring container.
+La documentacion abre en {host}:{puerto}/{contexto}/swagger-ui/index.html
+ */
+@Configuration
+//@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.gdrc.market.web.controller"))
+                .build();
+    }
+}
